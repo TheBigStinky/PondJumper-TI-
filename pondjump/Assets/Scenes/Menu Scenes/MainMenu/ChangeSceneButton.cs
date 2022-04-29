0ce126514d29545dff652efe75129ac418fbaf35
mtphadnis <its715am@gmail.com>
2022-04-28 23:34:52 -0400

    okay the sounds work now

--- pondjump/Assets/Scenes/Menu Scenes/MainMenu/ChangeSceneButton.cs
+++ pondjump/Assets/Scenes/Menu Scenes/MainMenu/ChangeSceneButton.cs
@@ -9,35 +9,107 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneButton : MonoBehaviour
 {
-	public Slider mousey;
-	public float valSlide;
+	
     private void Start()
     {
 		for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
 		{
 			Debug.Log("Scene " + i + ": " + SceneManager.GetSceneByBuildIndex(i).name);
 		}
-		
-		mousey.onValueChanged.AddListener(delegate { ChangeSlideValue(); });
+	
 	}
 
-    private void ChangeSlideValue()
-    {
-		valSlide = mousey.value;
-	}
 
-    public void PlaySound()
+	public void OnPlaySound()
 	{
-		playselect.PlayOneShot(selectbeep);
+		FindObjectOfType<AudioManager>().Play("PlayMM");
 	}
 
-	public void GoBackSound()
-    {
-		playselect.PlayOneShot(scrollbeep);
+	public void PressButton()
+	{
+		FindObjectOfType<AudioManager>().Play("ButtonPressGeneral");
+	}
+
+	public void GoBack()
+	{
+		FindObjectOfType<AudioManager>().Play("LillyClick");
+	}
+
+	public void HSMenu()
+	{
+
+
+
+		FindObjectOfType<AudioManager>().Play("HSClick");
+	}
+
+	public void Quit()
+	{
+		FindObjectOfType<AudioManager>().Play("Quit");
+	}
+
+
+	//Starts Environmental Sounds
+	public void GeneralAmb()
+	{
+		FindObjectOfType<AudioManager>().Play("General Ambiance");
+	}
+
+	public void AmbBurbs()
+	{
+		FindObjectOfType<AudioManager>().Play("AmbientSuburbs");
+	}
+
+	public void BreezeGrass()
+	{
+		FindObjectOfType<AudioManager>().Play("breezeandgrass");
+	}
+
+	public void FactoryAmb()
+	{
+		FindObjectOfType<AudioManager>().Play("FactoryAmbiance");
+	}
+
+	public void FrogCroak()
+	{
+		
+		FindObjectOfType<AudioManager>().Play("FrogCroak");
+	}
+
+	public void EveRain()
+	{
+		FindObjectOfType<AudioManager>().Play("Rain");
+	}
+	public void SoftWind()
+	{
+		FindObjectOfType<AudioManager>().Play("Soft-Wind");
+	}
+
+	public void WatDrips()
+	{
+	
+		FindObjectOfType<AudioManager>().Play("WaterDrips");
+	}
+
+	
+	public void CaveWat()
+	{
+		FindObjectOfType<AudioManager>().Play("WaterInCave");
+	}
+
+	public void WellWat()
+	{
+		FindObjectOfType<AudioManager>().Play("WellWater");
+	}
+
+	public void VentWind()
+	{
+		
+		FindObjectOfType<AudioManager>().Play("WindInVent");
 	}
 	public void LoadScene(int sceneName)
 	{
-		playselect.PlayOneShot(selectbeep);
+		
 		change = sceneName;
 		Invoke("ChangeScene", 2f);
 		Debug.Log(change);
@@ -62,8 +134,7 @@ public class ChangeSceneButton : MonoBehaviour
 
 
 	public int change;
-	public AudioSource playselect;
-	public AudioClip scrollbeep;
-	public AudioClip selectbeep;
 
 }
+
+
--- pondjump/Assets/Scripts/thirdSoul.cs
+++ pondjump/Assets/Scripts/thirdSoul.cs
@@ -13,12 +13,7 @@ public class thirdSoul : MonoBehaviour
     private Rigidbody rigidBody;
     private GameObject mainCamera;
     private runeBehavior runeBehavior;
-    public AudioClip runeCast;
-    public AudioSource source;
-    public AudioClip catchCast;
-    public AudioClip incorrectcast;
-    public AudioClip grappleshoot;
-    public AudioClip bounce;
+  
 
 
     [Header("Ground Detections")]
@@ -362,7 +357,7 @@ public class thirdSoul : MonoBehaviour
             GroundRadius = 0;
 
             rigidBody.AddForce(new Vector3(controller.velocity.x, JumpHeight, controller.velocity.z), ForceMode.Impulse);
-            source.PlayOneShot(bounce);
+            
 
         }
         else if (context.canceled)
@@ -424,7 +419,7 @@ public class thirdSoul : MonoBehaviour
                     LaunchIconsPlaced[launchScroll].SetActive(true);
                 }
 
-                //source.PlayOneShot(catchCast);
+                
 
             }
             else { crosshairBase.GetComponent<Image>().color = new Color32(255, 0, 0, 255); }
